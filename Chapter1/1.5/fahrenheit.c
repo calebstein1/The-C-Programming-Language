70c9dd416d5b07e051b66d06f309a26c4d75f160
Caleb Stein <caleb@calebstein.net>
2024-02-12 14:12:38 -0800

    Exercise 1.5

---
+++ Chapter1/1.5/fahrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int fahr;
+
+  printf("Fahrenheit Celsius\n");
+  for (fahr = 300; fahr >= 0; fahr -= 20)
+    printf("%10d%8.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+
+  return 0;
+}
